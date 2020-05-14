277c3966744d7547ee59b5b3fda0dfe570ff4758
Michal Fojtik <mfojtik@redhat.com>
2020-05-14 09:37:43 +0200

    restart operator on config change

--- pkg/cmd/operator/cmd.go
+++ pkg/cmd/operator/cmd.go
@@ -3,7 +3,10 @@ package operator
 import (
 	"context"
 	"io/ioutil"
+	"os"
+	"time"
 
+	"github.com/openshift/library-go/pkg/controller/fileobserver"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 	"k8s.io/klog"
@@ -12,6 +15,23 @@ import (
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/config"
 )
 
+func restartOnConfigChange(ctx context.Context, path string, startingContent []byte) {
+	observer, err := fileobserver.NewObserver(1 * time.Second)
+	if err != nil {
+		panic(err)
+	}
+	if len(startingContent) == 0 {
+		klog.Warningf("No configuration file available")
+	}
+	observer.AddReactor(func(file string, action fileobserver.ActionType) error {
+		os.Exit(0)
+		return nil
+	}, map[string][]byte{
+		path: startingContent,
+	}, path)
+	observer.Run(ctx.Done())
+}
+
 func NewOperator(ctx context.Context) *cobra.Command {
 	var configPath string
 
@@ -19,10 +39,8 @@ func NewOperator(ctx context.Context) *cobra.Command {
 		Use:   "run",
 		Short: "Run the operator",
 		Run: func(cmd *cobra.Command, args []string) {
-			configBytes, err := ioutil.ReadFile(configPath)
-			if err != nil {
-				klog.Fatalf("Unable to read config %q: %v", configPath, err)
-			}
+			configBytes, _ := ioutil.ReadFile(configPath)
+			go restartOnConfigChange(ctx, configPath, configBytes)
 			c := &config.OperatorConfig{}
 			if err := yaml.Unmarshal(configBytes, c); err != nil {
 				klog.Fatalf("Unable to parse config: %v", err)
