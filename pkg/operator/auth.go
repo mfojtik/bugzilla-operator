885a2bef183d8788623e31062680c19e23e287bc
Michal Fojtik <mfojtik@redhat.com>
2020-05-19 16:58:02 +0200

    do more debugging on event user

--- pkg/operator/auth.go
+++ pkg/operator/auth.go
@@ -18,7 +18,7 @@ func auth(cfg config.OperatorConfig, handler func(req slacker.Request, w slacker
 
 	return func(req slacker.Request, w slacker.ResponseWriter) {
 		if !users.Has(req.Event().Username) {
-			w.Reply(fmt.Sprintf("Permission denied: User %q does not have permission to run this command", req.Event().Username))
+			w.Reply(fmt.Sprintf("Permission denied: User %q (%q) does not have permission to run this command", req.Event().Username, req.Event().User))
 		}
 	}
 }
