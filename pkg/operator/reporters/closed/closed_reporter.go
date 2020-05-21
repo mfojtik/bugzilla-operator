d5097f3aad073a069ec8f6a3725cd891eaa0949d
Michal Fojtik <mfojtik@redhat.com>
2020-05-21 14:25:41 +0200

    sort the closed bugs report output by resolutions

--- pkg/operator/reporters/closed/closed_reporter.go
+++ pkg/operator/reporters/closed/closed_reporter.go
@@ -3,11 +3,13 @@ package closed
 import (
 	"context"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/eparis/bugzilla"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/events"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/bugutil"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/config"
@@ -15,16 +17,16 @@ import (
 )
 
 type BlockersReporter struct {
-	config      config.OperatorConfig
+	config            config.OperatorConfig
 	newBugzillaClient func() bugzilla.Client
-	slackClient slack.ChannelClient
+	slackClient       slack.ChannelClient
 }
 
 func NewClosedReporter(operatorConfig config.OperatorConfig, scheduleInformer factory.Informer, newBugzillaClient func() bugzilla.Client, slackClient slack.ChannelClient, recorder events.Recorder) factory.Controller {
 	c := &BlockersReporter{
-		config:      operatorConfig,
+		config:            operatorConfig,
 		newBugzillaClient: newBugzillaClient,
-		slackClient: slackClient,
+		slackClient:       slackClient,
 	}
 	return factory.New().WithSync(c.sync).WithInformers(scheduleInformer).ToController("BlockersReporter", recorder)
 }
@@ -59,23 +61,33 @@ func Report(ctx context.Context, client bugzilla.Client, recorder events.Recorde
 		resolutionMap[bug.Resolution] = append(resolutionMap[bug.Resolution], bug)
 	}
 
-	message := []string{}
+	messageMap := map[string][]string{}
+	resolutions := sets.NewString()
 	for resolution, bugs := range resolutionMap {
 		ids := []string{}
 		for _, b := range bugs {
 			ids = append(ids, fmt.Sprintf("<https://bugzilla.redhat.com/show_bug.cgi?id=%d|#%d>", b.ID, b.ID))
 		}
-		p := "bugs"
-		if len(bugs) == 1 {
-			p = "bug"
+		if messageMap[resolution] == nil {
+			messageMap[resolution] = []string{}
 		}
-		message = append(message, fmt.Sprintf("> %d %s closed as _%s_ (%s)", len(bugs), p, resolution, strings.Join(ids, ",")))
+		messageMap[resolution] = append(messageMap[resolution], fmt.Sprintf("> %s closed as _%s_ (%s)", bugutil.BugCountPlural(len(bugs), false), resolution, strings.Join(ids, ",")))
+		if !resolutions.Has(resolution) {
+			resolutions.Insert(resolution)
+		}
+	}
+
+	sortedResolutions := resolutions.List()
+	sort.Strings(sortedResolutions)
+	var messages []string
+	for _, resolution := range sortedResolutions {
+		messages = append(messages, messageMap[resolution]...)
 	}
 
 	if len(closedBugs) == 0 {
 		return "", nil
 	}
 
-	report := fmt.Sprintf("*%s Closed in the last 24h*:\n%s\n", bugutil.BugCountPlural(len(closedBugs), true), strings.Join(message, "\n"))
+	report := fmt.Sprintf("*%s Closed in the last 24h*:\n%s\n", bugutil.BugCountPlural(len(closedBugs), true), strings.Join(messages, "\n"))
 	return report, nil
 }
