dfd89d0cc6c928494f4c79d821aebc855cba5631
Michal Fojtik <mfojtik@redhat.com>
2020-09-25 13:06:32 +0200

    reassign: add reporter

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -27,6 +27,7 @@ import (
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/reporters/closed"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/reporters/escalation"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/reporters/incoming"
+	"github.com/mfojtik/bugzilla-operator/pkg/operator/reporters/reassign"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/reporters/upcomingsprint"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/resetcontroller"
 	"github.com/mfojtik/bugzilla-operator/pkg/operator/stalecontroller"
@@ -117,6 +118,8 @@ func Run(ctx context.Context, cfg config.OperatorConfig) error {
 			return incoming.NewIncomingReporter(ctx, when, cfg, recorder)
 		case "incoming-stats":
 			return incoming.NewIncomingStatsReporter(ctx, when, cfg, recorder)
+		case "moved-bugs":
+			return reassign.NewReassignReporter(ctx, when, cfg, recorder)
 		case "closed-bugs":
 			return closed.NewClosedReporter(ctx, components, when, cfg, recorder)
 		case "upcoming-sprint":
@@ -243,6 +246,11 @@ func Run(ctx context.Context, cfg config.OperatorConfig) error {
 				"incoming-stats": func(ctx context.Context, client cache.BugzillaClient) (string, error) {
 					// TODO: restrict components to one team
 					report, err := incoming.ReportStats(ctx, controllerContext, recorder, &cfg)
+					return report, err
+				},
+				"moved-bugs": func(ctx context.Context, client cache.BugzillaClient) (string, error) {
+					// TODO: restrict components to one team
+					report, err := reassign.Report(ctx, controllerContext, recorder, &cfg)
 					return report, err
 				},
 				"upcoming-sprint": func(ctx context.Context, client cache.BugzillaClient) (string, error) {
